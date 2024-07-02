39685cab7362f253141ae00372051219a6c654fd
Andrzej Świętek <mr.andi321@gmail.com>
2024-07-02 21:45:13 +0200

    Introduced Spring Boot as Backend

---
+++ backend/src/main/java/pl/swietek/elevatorsystem/ElevatorsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.swietek.elevatorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/swietek/elevatorsystem/ElevatorsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.swietek.elevatorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
