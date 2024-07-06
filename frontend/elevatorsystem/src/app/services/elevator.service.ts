716bc569ab8774f88f307e728f6549d30784c818
Andrzej Świętek <mr.andi321@gmail.com>
2024-07-06 13:03:52 +0200

    Introduced ElevatorSimulation and Elevator services on frontend

---
+++ frontend/elevatorsystem/src/app/services/elevator.service.ts
@@ -0,0 +1,29 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IElevatorData, IElevatorStatus} from "../shared/types";
+import {IElevatorService} from "../shared/types/IElevatorService";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ElevatorService implements IElevatorService{
+
+  private URL: string = 'http://localhost:8080/elevator';
+  private http: HttpClient = inject(HttpClient);
+  public constructor() { }
+
+  public getAllElevators(): Observable<IElevatorData[]> {
+    return this.http.get(`${this.URL}/all`);
+  }
+
+
+  public getAllElevatorsStatuses(): Observable<IElevatorStatus[]> {
+    return this.http.get<IElevatorStatus[]>(`${this.URL}/all-statuses`);
+  }
+
+
+  public getElevatorById(id: number): Observable<IElevatorData> {
+    return this.http.get(`${this.URL}/${id}`);
+  }
+}
